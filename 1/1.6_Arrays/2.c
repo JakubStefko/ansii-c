29a3bdc7526d8eaad1ce591820ced024a04df2a8
Jakub Stefko <jstefko@man.poznan.pl>
2020-05-09 00:25:47 +0200

    1.6 done

--- 1/1.6_Arrays/2.c
+++ 1/1.6_Arrays/2.c
@@ -8,19 +8,20 @@ void main()
 {
   int c, i, j, min, max;
   // my digits
-  int digit_number[10] = {1, 7, 9, 1, 8, 3, 1, 2, 0, 5};
+  int digit_number[10] = {2, 7, 9, 7, 8, 3, 4, 2, 0, 5};
 
   min = __INT32_MAX__;
   max = -__INT32_MAX__ - 1;
 
   for (i = 0; i < 10; i++)
-  {
-    if (digit_number[i] < min)
-      min = digit_number[i];
+    if (digit_number[i] > 0)
+    {
+      if (digit_number[i] < min)
+        min = digit_number[i];
 
-    if (digit_number[i] > max)
-      max = digit_number[i];
-  }
+      if (digit_number[i] > max)
+        max = digit_number[i];
+    }
 
   for (i = max; i >= min; i--)
   {
--- 1/1.6_Arrays/3.c
+++ 1/1.6_Arrays/3.c
@@ -1,3 +1,58 @@
 /*
   Make histogram for any character
 */
+
+#include <stdio.h>
+
+void main()
+{
+  int i, j, max, min;
+  int ascii[256];
+
+  // initializing empty ascii array
+  for (i = 0; i < 256; i++)
+    ascii[i] = 0;
+
+  // filling it with some values to show histogram
+  ascii[65] = 9;  // 'A'
+  ascii[72] = 3;  // 'H'
+  ascii[91] = 7;  // '['
+  ascii[106] = 2; // 'j'
+  ascii[50] = 3;  // '2'
+
+  // defining min and max on histogram
+  min = __INT32_MAX__;
+  max = -1;
+
+  for (i = 0; i < 256; i++)
+    if (ascii[i] > 0)
+    {
+      if (ascii[i] < min && ascii[i] > 0)
+        min = ascii[i];
+
+      if (ascii[i] > max)
+        max = ascii[i];
+    }
+
+  // showing actual histogram
+  for (i = max; i >= min; i--)
+  {
+    printf("%3d |", i);
+
+    for (j = 0; j < 256; j++)
+      if (ascii[j] > 0)
+        if (ascii[j] < i)
+          printf("  ");
+        else
+          printf(" x");
+    printf("\n");
+  }
+
+  printf("     ");
+
+  for (i = 0; i < 256; i++)
+    if (ascii[i] > 0)
+      printf(" %c", i);
+
+  printf("\n");
+}
